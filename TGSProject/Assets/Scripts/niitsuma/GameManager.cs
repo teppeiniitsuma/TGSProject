26b9a3ecc99842ec12f0f468b241b6cd85dcd748
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-05-29 15:43:38 +0900

    GameManagerクラス仮定義

---
+++ TGSProject/Assets/Scripts/niitsuma/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public enum GameState
+    {
+        Title,
+        StageSelect,
+        Main,
+        GameOver,
+        Result
+    }
+    public GameState gameState;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+        }
+    }
+}
