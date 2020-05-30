4070cba66e52f7cc7f4f2af029b6644c455d57bd
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2020-05-30 15:53:57 +0900

    シーン転移関係
    配置など作っていないが
    コードだけ用意しました

---
+++ TGSProject/Assets/Scripts/zhou/StageConsole.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+static public class StageConsole
+{
+    public enum MyScene
+    {
+        Title,
+        StageSelect,
+        Stage1,
+        Stage2,
+        Stage3,
+        Stage4,
+        Result,
+        GameOver
+    }
+    public static MyScene scene;
+    static Dictionary<string, MyScene> sceneDic = new Dictionary<string, MyScene>() {
+    {"Title", MyScene.Title },
+    {"StageSelect",     MyScene.StageSelect },
+    {"Stage1",   MyScene.Stage1 },
+    {"Stage2",   MyScene.Stage2 },
+    {"Stage3",   MyScene.Stage3 },
+    {"Stage4",   MyScene.Stage4 },
+    {"Result",   MyScene.Result },
+    {"GameOver",   MyScene.GameOver }
+};
+
+    public static MyScene MyGetScene()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        scene = sceneDic[sceneName];
+        return scene;
+
+    }
+    // enumのシーンで指定したシーンをロードする
+    public static void MyLoadScene(MyScene scene)
+    {
+        SceneManager.LoadScene(sceneDic.FirstOrDefault(x => x.Value == scene).Key);
+    }
+  
+}
+/*
+// 現在のシーンを再度ロードする
+public void ReloadCurrentSchene()
+{
+    StageConsole.MyScene scene = StageConsole.MyGetScene();
+    StageConsole.MyLoadScene(scene);
+}
+
+
+// 現在のシーンの次のシーンに遷移する
+
+
+
+/*
+          StageName stageName= StageName.Title;
+        //  stageName = StageName.Title++;
+        // stageName++;
+        //　変数ｂを宣言し　　データの種類　データ名　データ番号
+        var b = (StageName)1;
+        Debug.Log(b);
+       
+        //  Debug.Log(stageName);
+        */
+//public static MyScene scene;
+// シーン名とenumのシーンとを対応させる
---
+++ TGSProject/Assets/Scripts/zhou/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    //-------------------------------------------------------------------------------
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+      
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StageMove.ReloadCurrentSchene();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StageMove.LoadNextSchene();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            StageMove.LoadGameOverSchene();
+        }
+
+    }
+}
