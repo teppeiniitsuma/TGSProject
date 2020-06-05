c350a968304c6b13081e6688b65b8ce64ba5ea37
RenPrismaTokiwagrorb <mystia-lorelei.2bos@ezweb.ne.jp>
2020-06-05 14:58:10 +0900

    プレイ時間の表示を追加

---
+++ TGSProject/Assets/Scripts/yamazaki/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float countTime = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countTime += Time.deltaTime; // スタートしてからの秒数を格納
+
+        GetComponent<Text>().text = countTime.ToString("F2"); // 少数2桁にして表示
+    }
+}
