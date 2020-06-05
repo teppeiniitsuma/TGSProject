4d3a527cfa9a62d86d4f63a4ead8ea9f2cc343ce
arufa2530 <moriken.0325@gmail.com>
2020-06-05 16:05:24 +0900

    移動関数の作成

---
+++ TGSProject/Assets/Scripts/mori/BaseEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour
+{
+    //  Enemyの体力
+    public int Hp;
+
+    //  EnemyにPlayerを認識させる
+    public GameObject player;
+
+    private void DamageCollider(Collider2D collider)
+    {
+
+    }
+
+    private void AttackCollider(Collider2D collider)
+    {
+
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    private void Confirmation()
+    {
+
+    }
+
+    private void DeleteEnemy()
+    {
+
+    }
+
+
+}
