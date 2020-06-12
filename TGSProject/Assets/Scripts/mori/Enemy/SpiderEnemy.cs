7cd513dcac73007e966f93994afd8d1c1cc7e1d1
arufa2530 <moriken.0325@gmail.com>
2020-06-12 16:44:21 +0900

    確認のための一時上げ

--- TGSProject/Assets/Scripts/mori/Enemy/SpiderEnemy.cs
+++ TGSProject/Assets/Scripts/mori/Enemy/SpiderEnemy.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class SpiderEnemy : BaseEnemy 
 {
+    //  オブジェクトのRigidbodyの変数
+    //private Rigidbody2D ri2d;
+    //  playerのtransformを格納する変数
+    [SerializeField]
+    private Transform player;
+    //  オブジェクトの移動速度を格納する変数
+    [SerializeField]
+    private float moveSpeed;
+    //  オブジェクトとplayerの適切な距離で停止する変数
+    [SerializeField]
+    private float stopMove;
+    //  playerがオブジェクトに近づいたら開始する変数
+    [SerializeField]
+    private float startMove;
 
-    private Rigidbody2D ri2d;
-    //  trueならば敵を追い続ける画面が変わったらfalseになる
-    //private bool ScreenSwitchingTarget = true;
-
-    // Start is called before the first frame update
     void Start()
     {
-        ri2d = GetComponent<Rigidbody2D>();
+        //ri2d = GetComponent<Rigidbody2D>();
         //  
-        player = GameObject.Find("player");
+        //player = GameObject.Find("player");
     }
 
     // Update is called once per frame
@@ -26,16 +35,30 @@ public class SpiderEnemy : BaseEnemy
     //  移動関数
     private void Move()
     {
-        //if(ScreenSwitchingTarget == true)
-        //{
-            Vector2 tagetPos = player.transform.position;
+        Vector2 tagetPos = player.position;
 
-            float x = tagetPos.x;
-            float y = 0;
+        //tagetPos.y = transform.position.y;
 
-            Vector2 direction = new Vector2(x - transform.position.x, y).normalized;
-            ri2d.velocity = direction * 2;
-        //}
+        transform.LookAt(tagetPos);
+
+        float move = Vector2.Distance(transform.position, player.position);
+
+        if(move < startMove && move > stopMove)
+        {
+            transform.position = transform.position * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    //  移動関数
+    private void Move2()
+    {
+            //Vector2 tagetPos = player.transform.position;
+
+            //float x = tagetPos.x;
+            //float y = 0;
+
+            //Vector2 direction = new Vector2(x - transform.position.x, y).normalized;
+            //ri2d.velocity = direction * 2;
     }
 
 }
