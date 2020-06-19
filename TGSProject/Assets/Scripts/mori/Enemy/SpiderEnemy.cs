6e7b6a21bbcba388d91b653c87a446b8415b60e2
arufa2530 <moriken.0325@gmail.com>
2020-06-19 19:24:24 +0900

    敵の反復移動の実装

--- TGSProject/Assets/Scripts/mori/Enemy/SpiderEnemy.cs
+++ TGSProject/Assets/Scripts/mori/Enemy/SpiderEnemy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpiderEnemy : BaseEnemy 
 {
@@ -11,22 +12,39 @@ public class SpiderEnemy : BaseEnemy
     [SerializeField]
     private Transform player;
     //  オブジェクトの移動速度を格納する変数
-    //[SerializeField]
-    //private float moveSpeed;
+    [SerializeField]
+    private float attMove;
     //  オブジェクトとplayerの適切な距離で停止する変数
     [SerializeField]
     private float stopMove;
     //  playerがオブジェクトに近づいたら開始する変数
     [SerializeField]
     private float startMove;
+    [SerializeField]
+    private float enemyRange_1;
+    [SerializeField]
+    private float enemyRange_2;
+    [SerializeField]
+    private float time = 3f;
     private bool playerConfirmation = false;
+    private bool enemyConfirmation = false;
+    private Vector2 startPosition;
+    private Vector2 endPosition;
+    private Vector2 position;
+    private Vector2 vec;
+    [SerializeField]
+    private float position_max = 2f;
+    [SerializeField]
+    private float position_mix = 2f;
+    //float conten;
 
     void Start()
     {
         ri2d = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
+        //conten = transform.position.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
@@ -36,8 +54,16 @@ public class SpiderEnemy : BaseEnemy
     //  プレイヤーを検知する関数
     private void Confirmation()
     {
-        Vector3 playerPos = player.position;
-        float distance = Vector3.Distance(transform.position, player.position);
+        //endPosition.x = startPosition.x + enemyRange_1;
+        //position.x = startPosition.x + enemyRange_2;
+
+        //  プレイヤーのトランスフォームを取る
+        Vector2 playerPos = player.position;
+        //  自分positionとプレイヤーのpositionをdistanceに入れる
+        float distance = Vector2.Distance(transform.position, playerPos);
+        //float pos = Vector2.Distance(transform.position, endPosition);
+        //float pos2 = Vector2.Distance(transform.position, position);
+        //  指定した範囲内にプレイヤーが居るときはtrue、居ないときはfalse
         if(distance < startMove && distance > stopMove)
         {
             playerConfirmation = true;
@@ -47,6 +73,15 @@ public class SpiderEnemy : BaseEnemy
         {
             playerConfirmation = false;
             Debug.Log("プレイヤーが見つかりません。");
+            //if() 
+            //{
+            //    Debug.Log("範囲にいます");
+
+            //}
+            //else
+            //{
+            //    Debug.Log("範囲に居ません");
+            //}
         }
 
     }
@@ -54,26 +89,60 @@ public class SpiderEnemy : BaseEnemy
     //  移動関数
     private void Move()
     {
-        if(playerConfirmation == true)
+        
+        if(playerConfirmation)//    プレイヤーが範囲内に居るとき
         {
-
+            Move2();
         }
-        else if(playerConfirmation == false)
+        else if(!playerConfirmation)//  プレイヤーが範囲内に居ないとき
         {
-
+            
+            if(enemyConfirmation)// エネミーが最初の範囲に居るとき
+            {
+                
+            }
+            else if(!enemyConfirmation)//   エネミーが最初の範囲に居ないとき
+            {
+                transform.position = new Vector2(Mathf.MoveTowards
+                (transform.position.x, startPosition.x, Time.deltaTime), transform.position.y);
+                aho_move();
+            }
+            // transform.position.z);
+           
         }
     }
 
     //  移動関数
     private void Move2()
     {
-            //Vector2 tagetPos = player.transform.position;
+        Vector2 tagetPos = player.transform.position;
 
-            //float x = tagetPos.x;
-            //float y = 0;
+        float x = tagetPos.x;
+        float y = 0;
 
-            //Vector2 direction = new Vector2(x - transform.position.x, y).normalized;
-            //ri2d.velocity = direction * 2;
+        Vector2 direction = new Vector2(x - transform.position.x, y).normalized;
+        ri2d.velocity = direction * 2;
+    }
+
+    private void aho_move()
+    {
+        startPosition.x += Time.deltaTime * time;
+        if(startPosition.x >= position_max)
+        {
+            time *= -1;
+            startPosition.x = position_max;
+        }
+        else if(startPosition.x <= position_mix)
+        {
+            time *= -1;
+            startPosition.x = position_mix;
+        }
+        transform.position = new Vector2(startPosition.x, -1.5f);
     }
 
 }
+
+/*
+ 
+ 
+ */
