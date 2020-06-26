c745252d613c68f2149094ef7924a12d17c3dbc8
arufa2530 <moriken.0325@gmail.com>
2020-06-26 18:59:58 +0900

    タイトル画面のPress 〇 to start が点滅するように変更

--- TGSProject/Assets/Scripts/mori/UI/Scripts/Fluffy.cs
+++ TGSProject/Assets/Scripts/mori/UI/Scripts/Fluffy.cs
@@ -6,14 +6,9 @@ using UnityEngine.UI;
 public class Fluffy : MonoBehaviour
 {
     private Vector2 logo;
-    [SerializeField]
-    [Header("↓↓ロゴのふり幅")]
-    [Range(0.0f, 1.0f)]
-    private float logo_time = 0.15f;
-    [SerializeField]
-    [Header("↓↓ロゴの速度")]
-    [Range(0.0f, 5.0f)]
-    private float loge_speed = 1.0f;
+    [SerializeField][Header("↓↓ロゴのふり幅")][Range(0.0f, 1.0f)]private float logo_time = 0.15f;
+    [SerializeField][Header("↓↓ロゴの速度")][Range(0.0f, 5.0f)]private float loge_speed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ TGSProject/Assets/Scripts/mori/UI/Scripts/PeseeStart.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeseeStart : MonoBehaviour
+{
+    private bool yes;
+    private float alpha;
+    private float red, green, blue;
+    private float Max = 1.0f;
+    [SerializeField] [Header("↓↓0に近づけるほど透明になる")] [Range(0.0f, 1.0f)] private float Mix;
+    [SerializeField] [Header("↓↓点滅する速度")] private float speed;
+
+    void Start()
+    {
+        alpha = GetComponent<SpriteRenderer>().color.a;
+        red = GetComponent<SpriteRenderer>().color.r;
+        green = GetComponent<SpriteRenderer>().color.g;
+        blue = GetComponent<SpriteRenderer>().color.b;
+        alpha = 1.0f;
+        GetComponent<SpriteRenderer>().color = new Color(red, green, blue, alpha);
+        yes = false;
+    }
+
+    private void Aho()
+    {
+        if(alpha < Mix) { yes = true; }
+        else if(alpha > Max) { yes = false; }
+    }
+
+    void Update()
+    {
+        Aho();
+        if(yes)
+        {
+            alpha = alpha + Time.deltaTime * speed;
+            GetComponent<SpriteRenderer>().color = new Color(red, green, blue, alpha);
+        }
+        else if(!yes)
+        {
+            alpha = alpha + Time.deltaTime * -speed;
+            GetComponent<SpriteRenderer>().color = new Color(red, green, blue, alpha);
+        }
+    }
+}
