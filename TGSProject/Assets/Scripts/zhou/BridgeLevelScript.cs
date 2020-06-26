2597e17aa54226aa67be4c139f79c7b435de2c60
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2020-06-26 10:58:44 +0900

    ギミック＿橋の実装

---
+++ TGSProject/Assets/Scripts/zhou/BridgeLevelScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeLevelScript : MonoBehaviour
+{
+    [SerializeField] GameObject gameObject;
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Debug.Log("もし入力が欲しい場合");
+            gameObject.GetComponent<BridgeScript>().OpenLevel();
+            
+        }
+    }
+}
---
+++ TGSProject/Assets/Scripts/zhou/BridgeScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeScript : MonoBehaviour
+{
+    [SerializeField] GameObject switch_Head,level;
+    public bool isSwitchUp,isLevel;
+    [SerializeField] int rotsSpeed = 180 ;
+    public float startRotsZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // startRotsZ = this.transform.rotation.z*120;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.rotation.z < 0 && isSwitchUp) {
+            this.transform.Rotate(0, 0, this.rotsSpeed * Time.deltaTime);
+            if (this.transform.rotation.z > 0) {
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+                if (isLevel) { 
+                //spot
+                }
+            }
+        } else if (this.transform.rotation.z> -40.0f && !isSwitchUp) {
+            this.transform.Rotate(0, 0, this.rotsSpeed * -Time.deltaTime);
+            if (this.transform.rotation.z < startRotsZ)
+            {
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, -40.0f);
+            }
+        }
+    }
+    public void OpenLevel() {
+
+        switch_Head.GetComponent<BridgeSwitchScript>().enabled = false;
+        level.GetComponent<BridgeLevelScript>().enabled = false;
+        isSwitchUp = true;
+        isLevel = true;
+
+
+    }
+}
