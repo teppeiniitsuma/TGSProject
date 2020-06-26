ed88d1e2bc1c1c85dcdc4575cf802341d7f26401
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-06-26 19:20:09 +0900

    タイム関連処理（後修正）

---
+++ TGSProject/Assets/Scripts/yamazaki/ElapseTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElapseTime : MonoBehaviour
+{
+    GameObject elapseTime;
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.elapseTime = GameObject.Find("elapseTime");
+    }
+
+    // 時間経過の度、少しずつ elapseTime を表示していく
+    public void IncreaseGage()
+    {
+        this.elapseTime.GetComponent<Image>().fillAmount += Time.deltaTime / speed;
+    }
+}
