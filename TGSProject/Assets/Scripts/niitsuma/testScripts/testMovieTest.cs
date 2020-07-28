117014e3b7c45825a175b86c0fb1fd2f506b137c
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-07-28 12:55:05 +0900

    コミットし忘れ

---
+++ TGSProject/Assets/Scripts/niitsuma/testScripts/testMovieTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage), typeof(VideoPlayer), typeof(AudioSource))]
+public class testMovieTest : MonoBehaviour
+{
+    RawImage image;
+    VideoPlayer player;
+    void Awake()
+    {
+        image = GetComponent<RawImage>();
+        player = GetComponent<VideoPlayer>();
+        var source = GetComponent<AudioSource>();
+        player.EnableAudioTrack(0, true);
+        player.SetTargetAudioSource(0, source);
+    }
+    void Update()
+    {
+        if (player.isPrepared)
+        {
+            image.texture = player.texture;
+        }
+    }
+}
