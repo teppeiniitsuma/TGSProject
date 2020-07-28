d79c02e151209991721617080f65cb6efcf89f11
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-07-28 11:00:38 +0900

    プレイヤーロード処理変更, 画像追加, ステージ１調整,  Gimmickプレハブ追加

---
+++ TGSProject/Assets/Scripts/niitsuma/Player/ReloadPositionSetter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ReloadPositionSetter : MonoBehaviour
+{
+    PlayerReload _reload;
+
+    void Start()
+    {
+        _reload = transform.parent.GetComponent<PlayerReload>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player") _reload.LoadPositionRewriting(transform);
+    }
+}
--- TGSProject/Assets/Scripts/zhou/InjurePlayerCollider.cs
+++ TGSProject/Assets/Scripts/zhou/InjurePlayerCollider.cs
@@ -1,21 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InjurePlayerCollider : MonoBehaviour
 {
-        void OnTriggerEnter2D(Collider2D collider)
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
         {
-            if (collider.tag == "Player")
-            {
-                Debug.Log("HP減り処理");
+            Debug.Log("HP減り処理");
             GameManager.Instance.Information.DecreaseHP();
-                //float x, y;
-                //-2 2
-                //-5 5
-                //if (collider.gameObject.transform.position.x > gameObject.transform.position.x) { x = 5; } else { x = -5; }
-                //if (collider.gameObject.transform.position.y > -1) { y = 2; } else { y=-2; }
-                //collider.transform.position = new Vector3(gameObject.transform.position.x+x,y,0);
-            }
         }
+    }
 }
