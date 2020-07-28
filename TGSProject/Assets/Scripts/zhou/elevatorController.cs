3739a73be11d81c014642e2cc2f5ca1155abacaa
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2020-07-28 19:05:54 +0900

    ステージ2のマップ配置
    未完成
    アーチ
    数字の謎機関

--- TGSProject/Assets/Scripts/zhou/elevatorController.cs
+++ TGSProject/Assets/Scripts/zhou/elevatorController.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class elevatorController : MonoBehaviour
+public class ElevatorController : MonoBehaviour
 {
+    //これは入力たびに動くエレベーター　かどうか
+    public bool isManual = false,isManualMove=false;
+
     [SerializeField] Material up, down, stop;
     [SerializeField] Material []material ;
     Material chain_rightMaterial, chain_leftMaterial;
     [SerializeField] GameObject chain_right, chain_left, floor, gear_Up, gear_down;
     [SerializeField] bool isMove, isUp;
     [SerializeField] float stopTime = 3, upSpeed = 1, rotsSpeed = 180, targetPos,targetUpPos = 1.2f, targetDownPos = -2.8f,time=0;
-
+    
     //-2.8   1.2
 
     // Start is called before the first frame update
@@ -18,13 +21,25 @@ public class elevatorController : MonoBehaviour
     {
         Rigidbody rb = floor.GetComponent<Rigidbody>();
         targetPos = targetUpPos;
+        //もし　少しランダムしかったら
+        if (isManual) { time = 0;
+            isMove = false;
+            isUp = false;
+        } else {
+            time = Random.Range(0.0f, 3.0f);
+        }
+     
+        upSpeed= upSpeed+ Random.Range(-2.0f, 2.0f);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(GameManager.Instance.GetGameState == GameManager.GameState.Main)
+       // if(GameManager.Instance.GetGameState == GameManager.GameState.Main)
         ElevatorMove();
+       
     }
     //エレベーターコントロール
     void ElevatorMove() {
@@ -42,9 +57,20 @@ public class elevatorController : MonoBehaviour
 
 
         }
-        else if (!isMove && time <= 0)
+        //自動式
+        else if (!isMove && time <= 0 && !isManual)
         {
-            if (isUp == true)
+            ManualMove();
+        }
+        //入力式
+        else if ((!isMove && time <= 0 && isManual&& isManualMove)) {
+            ManualMove();
+            isManualMove = false;
+        }
+           
+    }
+    void ManualMove() { 
+     if (isUp == true)
             {
                 isUp = false;
                 targetPos = targetDownPos;
@@ -58,13 +84,8 @@ public class elevatorController : MonoBehaviour
            
             isMove = true;
             ChainMaterialMove();
-        }
-
-       
-
         
     }
-
     //上げ下げる
     void ChainMaterialMove() {
         if (isUp && isMove)
