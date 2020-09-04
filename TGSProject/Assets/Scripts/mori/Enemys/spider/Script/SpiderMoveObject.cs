dbfe89a97e72fd3ce73afdba217cfa415994c4e5
arufa2530 <moriken.0325@gmail.com>
2020-09-04 10:14:49 +0900

    蜘蛛のおかしな挙動の修正とBaseEnemyの改良

---
+++ TGSProject/Assets/Scripts/mori/Enemys/spider/Script/SpiderMoveObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMoveObject : MonoBehaviour
+{
+    [SerializeField]
+    GameObject spider;
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x, spider.transform.position.y);
+    }
+}
