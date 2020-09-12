06fe4ed82f9ac3f0d20c335ab05dc4e3ecd224ed
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2020-09-12 12:01:31 +0900

    サウンド関係1

--- TGSProject/Assets/Scripts/zhou/GameSceneSoundTest.cs
+++ TGSProject/Assets/Scripts/zhou/GameSceneSoundTest.cs
@@ -9,11 +9,38 @@ public class GameSceneSoundTest : MonoBehaviour
     {
         //サウンド　リセット（展開でも言えるでしょう）
         SoundManager.init();
+
+
+        SoundManager.PlayMusic("Audios/title");//BGMを再生
+
+
+        SoundManager.PlayEffect("sounds/Close");//SE再生
+    
+        this.InvokeRepeating("Again", 3, 3);//3秒一回に
+
+
+
+    }
+    //AudioSourceを隠すテスト
+    void Again()
+    {
+         SoundManager.PlayEffect("Audios/title");
     }
 
-    // Update is called once per frame
-    void Update()
+
+    /*
+     SoundScan
+     
+      this.InvokeRepeating("Scan",0, 0.5f);
+     
+      void scan()
     {
-        
+        SoundManager.DisableOverAudio();//调用隐藏AudioSource组件接口
     }
+     
+     
+     */
+
+
+
 }
