084d2f44ce8a9123e7bb307fb3cf68a98928fe6e
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2020-09-18 12:39:07 +0900

    時計のＵＩ書き直し？
    時計のＵＩ書き直しTGSProject\Assets\Scripts\zhou\Zhou_Palette\TimerUI

---
+++ TGSProject/Assets/Scripts/zhou/PocketWatchManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// UI　時計
+/// </summary>
+public class PocketWatchManager : MonoBehaviour
+{
+    [Header("時計の影")]
+    [SerializeField]private Image image;
+    [Header("時計の針")]
+    [SerializeField] private RectTransform rectTransform;
+    [Header("時計の周り速度")]
+    [SerializeField] private float speed;
+    /// <summary>
+    /// 時計影と針ををリセット
+    /// </summary>
+    void PocketWatchReset()
+    {
+        image.fillAmount = 0;
+        rectTransform.rotation = Quaternion.Euler(0, 0, 0.0f);
+    }
+    void Update()
+    {
+        PocketWatchMove();
+    }
+    /// <summary>
+    /// 時計回る
+    /// </summary>
+    public void PocketWatchMove() {
+        image.fillAmount += Time.deltaTime / speed;
+        rectTransform.rotation = Quaternion.Euler(0, 0, -image.fillAmount * 360.0f);
+        if (image.fillAmount == 1.0f) {
+            Debug.Log("時計　もう一輪回りました。");
+        }
+    }
+}
