d2e4f01b716ce8f9a445930f5de374b2d65acaf8
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-09-24 16:14:00 +0900

    Revert "シナリオ再生用機能追加"
    This reverts commit 3cf43ee3716536088f7efcde89deb06b80cf7cf9.

--- TGSProject/Assets/Scripts/niitsuma/Scenarios/DialogMessageControl.cs
+++ TGSProject/Assets/Scripts/niitsuma/Scenarios/DialogMessageControl.cs
@@ -2,103 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using DualShockInput;
 
 public class DialogMessageControl : MonoBehaviour
 {
-	[SerializeField] Stage01_Start1 start1;
-	[SerializeField] Stage01_HerbGet herbGet;
-	[SerializeField] Stage01_Medusa medusa;
-	[SerializeField] Stage01_Stele stele;
-
 	[SerializeField] TutorialMessageData data;
+	//[SerializeField] Sample model;
+	[SerializeField] Text[] greenText = new Text[2];
+	[SerializeField] Text[] redText = new Text[2];
+	[SerializeField] Image[] dialogGreen = new Image[2];
+	[SerializeField] Image[] dialogRed = new Image[2];
 
-    #region ダイアログデータ（後で修正）
-    [SerializeField] Text[] green_L_Text = new Text[2];
-	[SerializeField] Text[] red_L_Text = new Text[2];
-	[SerializeField] Text[] green_M_Text = new Text[2];
-	[SerializeField] Text[] red_M_Text = new Text[2];
-
-	[SerializeField] Image[] dialog_L_Green = new Image[2];
-	[SerializeField] Image[] dialog_L_Red = new Image[2];
-	[SerializeField] Image[] dialog_M_Green = new Image[2];
-	[SerializeField] Image[] dialog_M_Red = new Image[2];
-
-	GameObject temp = null;
-	#endregion
-
-	public int SetScenarioID { set { id = value; } }
-	int id = 0;
+	
 	void Start()
 	{
 		//ShowMessage();
 	}
 
 	int count = 0;
-	List<TutorialData> tempData = new List<TutorialData>();
-	private void Update()
+    private void Update()
     {
-        if (DSInput.PushDown(DSButton.Circle) && GameManager.Instance.GetEventState == GameManager.EventState.ScenarioEvent ||
-			Input.GetKeyDown(KeyCode.Z) && GameManager.Instance.GetEventState == GameManager.EventState.ScenarioEvent)
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            switch (id)
-			{
-				case 1: tempData = start1.Message;  break;
-				case 2: tempData = herbGet.Message; break;
-				case 3: tempData = medusa.Message; break;
-				case 4: tempData = stele.Message; break;
+			//StartCoroutine(DialogPop(dialogGreen[count]));
+            if (data.Message[count].balloonColor == BalloonColor.GREEN)
+            {
+				StartCoroutine(DialogPop(dialogGreen[count]));
+				greenText[count].text = data.Message[count].message;
+				//switch (data.Message[count].balloonSize)
+				//{
+				//	case "S": break;
+				//	case "M": break;
+				//	case "L": break;
+				//}
 			}
-
-			if(count == tempData.Count) { GameManager.Instance.EventEnd(); count = 0; temp.SetActive(false); temp = null; return; }
-            if (tempData[count].balloonColor == BalloonColor.GREEN)
+            else if (data.Message[count].balloonColor == BalloonColor.RED)
             {
-				if(null != temp) { temp.SetActive(false); }
-				switch (tempData[count].balloonSize)
-                {
-                    case "S": 
-                    case "M":	if(tempData[count].balloonPos == "Left") {
-									StartCoroutine(DialogPop(dialog_M_Green[0]));
-									green_M_Text[0].text = tempData[count].message;
-								}
-								else if(tempData[count].balloonPos == "Right") {
-									StartCoroutine(DialogPop(dialog_M_Green[1]));
-									green_M_Text[1].text = tempData[count].message;
-								}	break;
-					case "L":	if(tempData[count].balloonPos == "Left") {
-									StartCoroutine(DialogPop(dialog_L_Green[0]));
-									green_L_Text[0].text = tempData[count].message;
-								}
-								else if(tempData[count].balloonPos == "Right") {
-									StartCoroutine(DialogPop(dialog_L_Green[1]));
-									green_L_Text[1].text = tempData[count].message;
-								}	break;
-				}
-            }
-            else if (tempData[count].balloonColor == BalloonColor.RED)
-            {
-				if (null != temp) { temp.SetActive(false); }
-				switch (tempData[count].balloonSize)
-                {
-                    case "S": 
-                    case "M":	if(tempData[count].balloonPos == "Left") {
-									StartCoroutine(DialogPop(dialog_M_Red[0]));
-									red_M_Text[0].text = tempData[count].message;
-								}
-								else if(tempData[count].balloonPos == "Right") {
-									StartCoroutine(DialogPop(dialog_M_Red[1]));
-									red_M_Text[1].text = tempData[count].message;
-								}	break;
-					case "L":	if(tempData[count].balloonPos == "Left") {
-									StartCoroutine(DialogPop(dialog_L_Red[0]));
-									red_L_Text[0].text = tempData[count].message;
-								}
-								else if(tempData[count].balloonPos == "Right") {
-									StartCoroutine(DialogPop(dialog_L_Red[1]));
-									red_L_Text[1].text = tempData[count].message;
-								}	break;
-                }
-            }
-            count = Mathf.Clamp(count + 1, 0, tempData.Count);
+				StartCoroutine(DialogPop(dialogRed[count]));
+				redText[count].text = data.Message[count].message;
+				//switch (data.Message[count].balloonSize)
+				//{
+				//    case "S": break;
+				//    case "M": break;
+				//    case "L": break;
+				//}
+			}
+            //greenText[count].text = data.Message[count].message;
+            count = Mathf.Clamp(count + 1, 0, greenText.Length - 1 /*data.Message.Count - 1*/);
         }
     }
 
@@ -106,7 +55,6 @@ public class DialogMessageControl : MonoBehaviour
 	IEnumerator DialogPop(Image d)
     {
 		d.gameObject.SetActive(true);
-		temp = d.gameObject;
 		Vector2 sizeMax = new Vector2(1, 1);
 		d.rectTransform.localScale = Vector2.zero;
 		while(d.rectTransform.localScale.x < sizeMax.x)
@@ -114,8 +62,8 @@ public class DialogMessageControl : MonoBehaviour
 			d.rectTransform.localScale = Vector2.MoveTowards(d.rectTransform.localScale, sizeMax, Time.deltaTime * 2);
 			yield return null;
 		}
-		//yield return new WaitForSeconds(1.0f);
-		//d.gameObject.SetActive(false);
+		yield return new WaitForSeconds(2.0f);
+		d.gameObject.SetActive(false);
 
 	}
  //   void ShowMessage()
