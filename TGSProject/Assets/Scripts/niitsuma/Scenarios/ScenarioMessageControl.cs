3cf43ee3716536088f7efcde89deb06b80cf7cf9
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-09-24 14:23:26 +0900

    シナリオ再生用機能追加

---
+++ TGSProject/Assets/Scripts/niitsuma/Scenarios/ScenarioMessageControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ScenarioMessageView))]
+public class ScenarioMessageControl : MonoBehaviour
+{
+    [SerializeField] Prologue prologue;
+    [SerializeField] Text _nameText;
+    [SerializeField] Text _messageText;
+
+    [SerializeField] private float _messageSpeed = 0.5f;
+    public float MessageSpeed { set { _messageSpeed = value; } }
+
+
+    private ScenarioMessageView _textView;
+    
+    private string _originalMessage = "";
+    private string _dispMessage = "";
+    private int _messageCount = 0;
+    private float _timer = 0;
+    private System.Action _callback = null;
+
+    public void SetMessage(string message, System.Action callback = null)
+    {
+        _callback = callback;
+        //_textType = textType;
+        _originalMessage = message;
+        _dispMessage = "";
+        _messageCount = 0;
+        _timer = 0;
+        StartCoroutine(MessageDisp());
+    }
+
+    private void Start()
+    {
+        _textView = GetComponent<ScenarioMessageView>();
+
+    }
+
+    IEnumerator MessageDisp()
+    {
+        while (_messageCount < _originalMessage.Length)
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= _messageSpeed)
+            {
+                _timer = 0;
+                _messageCount++;
+                // 元のメッセージから指定部分を引き出す(０～_messageCount)
+                _dispMessage = _originalMessage.Substring(0, _messageCount);
+                _textView.SetMessage(_dispMessage);
+            }
+            yield return null;
+        }
+        // ループを抜けたらcallback
+        if (_callback != null) _callback();
+    }
+}
---
+++ TGSProject/Assets/Scripts/niitsuma/Scenarios/ScenarioMessageView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScenarioMessageView : MonoBehaviour
+{
+    private Text _messageText;
+
+    void Start()
+    {
+        _messageText = GetComponent<Text>();
+    }
+
+    /// <summary>
+    /// メッセージテキストをセット
+    /// </summary>
+    /// <param name="message"></param>
+    public void SetMessage(string message)
+    {
+        _messageText.text = message;
+    }
+
+    /// <summary>
+    /// メッセージテキストをクリア
+    /// </summary>
+    public void ClearText()
+    {
+        _messageText.text = "";
+    }
+}
