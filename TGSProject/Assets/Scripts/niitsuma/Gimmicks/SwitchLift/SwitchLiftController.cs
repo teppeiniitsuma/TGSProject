ce095682ab65ae92e864d8f95a58a9271f287bc3
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-10-09 10:16:20 +0900

    ギミック関連修正及び追加

--- TGSProject/Assets/Scripts/niitsuma/Gimmicks/SwitchLift/SwitchLiftController.cs
+++ TGSProject/Assets/Scripts/niitsuma/Gimmicks/SwitchLift/SwitchLiftController.cs
@@ -2,62 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewLiftControl : MonoBehaviour
+public class SwitchLiftController : MonoBehaviour
 {
     [SerializeField] GameObject _gearUp, _gearDown;
     [SerializeField] Transform _floor;
-    [SerializeField] LiftChainMover _chain;
+    [SerializeField] SwitchChainMove _chain;
     [SerializeField] float _moveSpeed = 1, _rotsSpeed = 180, _floorUpPos = 1, _floorDownPos = 0, time = 0;
     [SerializeField] bool _isUp = false;
-    [SerializeField] bool _isInitialize = false;
 
+    public bool IsSwitch { get; set; } = false;
+    public bool IsLevel { get; set; } = false;
     public bool GetIsUp { get => _isUp; }
-    public bool IsMove { get { return _isMove; } set { _isMove = value; } }
+    public bool IsMove { get => _isMove; set => _isMove = value; }
     bool _isMove = false;
 
 
-    Vector3 startPos = Vector3.zero;
-    bool startIsUp = false;
-
-    void Awake()
-    {
-        startPos = _floor.position;
-        startIsUp = _isUp;
-    }
     void Update()
     {
-        if(GameManager.Instance.GetEventState == GameManager.EventState.GimmickEvent)
+        if (GameManager.Instance.GetEventState == GameManager.EventState.GimmickEvent)
         {
             LiftMove();
             GearRotate();
         }
 
-        if(GameManager.Instance.GetGameState == GameManager.GameState.Road && _isInitialize)
-        {
-            FloorInitialize();
-            _chain.ChainInitialize();
-        }
-    }
-
-    void FloorInitialize()
-    {
-        _floor.position = startPos;
-        _isUp = startIsUp;
     }
 
     void LiftMove()
     {
-        if(!_isUp && _isMove)
+        if (!_isUp && _isMove)
         {
-            _floor.position = new Vector2(_floor.position.x, 
+            _floor.position = new Vector2(_floor.position.x,
                 Mathf.MoveTowards(_floor.transform.position.y, _floorUpPos + transform.position.y, _moveSpeed * Time.deltaTime));
-            if(_floor.position.y == _floorUpPos + transform.position.y)
+            if (_floor.position.y == _floorUpPos + transform.position.y)
             {
                 _isUp = true; _isMove = false;
                 GameManager.Instance.EventEnd();
             }
         }
-        else if(_isUp && _isMove)
+        else if (_isUp && _isMove)
         {
             _floor.position = new Vector2(_floor.position.x,
                 Mathf.MoveTowards(_floor.transform.position.y, _floorDownPos + transform.position.y, _moveSpeed * Time.deltaTime));
@@ -67,7 +49,7 @@ public class NewLiftControl : MonoBehaviour
                 GameManager.Instance.EventEnd();
             }
         }
-        
+
     }
     // ギアを回す
     void GearRotate()
---
+++ TGSProject/Assets/Scripts/niitsuma/Gimmicks/SwitchLift/SwitchLiftFloorControl.cs
@@ -0,0 +1,70 @@
+﻿using DualShockInput;
+using UnityEngine;
+
+public class SwitchLiftFloorControl : MonoBehaviour
+{
+    [SerializeField] BoxCollider2D _floor;
+    [SerializeField] SwitchLiftController _lift;
+    [SerializeField] GameObject _actionUI;
+    [SerializeField] GameObject _playerParent; // プレイヤーの親オブジェ
+
+
+    // 上に乗ったらプラスする（これでトリガー判定を制御）
+    int _count = 0;
+    bool _isDisp;
+
+    // ロードに入った時に位置を初期化するか
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            _floor.isTrigger = true;
+            _count++;
+            collider.gameObject.transform.parent = transform.parent;
+            _isDisp = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            _count--;
+            if (_count == 0)
+            {
+                _isDisp = false;
+                _floor.isTrigger = false;
+                // 指定オブジェの子要素にする
+                collider.gameObject.transform.parent = _playerParent.transform;
+            }
+            else if (_count == 1)
+            {
+                collider.gameObject.transform.parent = _playerParent.transform;
+            }
+
+        }
+    }
+
+    void Update()
+    {
+        if (!_lift.IsMove)
+        {
+            if (_isDisp) { _actionUI.SetActive(true); }
+            else { _actionUI.SetActive(false); }
+        }
+        else
+        {
+            _actionUI.SetActive(false);
+        }
+
+        if (_isDisp && Input.GetKeyDown(KeyCode.Z) || DSInput.PushDown(DSButton.Circle) && _isDisp)
+        {
+            if(_lift.IsLevel || _lift.IsSwitch)
+            {
+                GameManager.Instance.SetEventState(GameManager.EventState.GimmickEvent);
+                _lift.IsMove = true;
+            }
+        }
+    }
+}
