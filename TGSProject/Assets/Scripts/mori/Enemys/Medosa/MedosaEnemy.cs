354f9dfbaf12a37b73e2e18f403042eb15dd99fd
arufa2530 <moriken.0325@gmail.com>
2020-10-12 12:52:17 +0900

    メデューサのバグ修正

--- TGSProject/Assets/Scripts/mori/Enemys/Medosa/MedosaEnemy.cs
+++ TGSProject/Assets/Scripts/mori/Enemys/Medosa/MedosaEnemy.cs
@@ -20,7 +20,7 @@ public class MedosaEnemy : BaseEnemy
     {
         if(iti)
         {
-            transform.localPosition = new Vector2(startPosition.x, startPosition.y);
+            transform.position = new Vector2(startPosition.x, startPosition.y);
             iti = !iti;
         }
     }
@@ -29,7 +29,7 @@ public class MedosaEnemy : BaseEnemy
     {
         if (!iti)
         {
-            transform.localPosition = new Vector2(startPosition.x + direction_x, startPosition.y);
+            transform.position = new Vector2(startPosition.x + direction_x, startPosition.y);
             iti = !iti;
         }
     }
