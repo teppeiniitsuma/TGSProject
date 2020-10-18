c45930b26b6fe1ca0a36505a37a9f61bc7788748
arufa2530 <moriken.0325@gmail.com>
2020-10-18 23:34:28 +0900

    NormalEnd、TrueEndの仮作成

---
+++ TGSProject/Assets/Scripts/mori/UI/Movie/Script/LastTrueMovie.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class LastTrueMovie : MonoBehaviour
+{
+    [SerializeField] FadeController _faUI;
+    [SerializeField]GameObject lastBossSpider;
+    [SerializeField] GameObject medosa;
+    [SerializeField] CollectedButterfly butterfly;
+    //LastEnemy lsEnemy;
+    private bool _videoPlay;
+    public bool _videoStop { get; set; }
+    float _deletionTime;
+    void Start()
+    {
+        //lsEnemy = lastBossSpider.GetComponent<LastEnemy>();
+        _videoPlay = false;
+        _deletionTime = 38f;
+    }
+
+    private void BossVictory()
+    {
+        
+        if(/*0 == lsEnemy.lastBossHp && */!_videoPlay)
+        {
+            _faUI.Fade(false, () => Deletion());            
+            _videoPlay = true;
+        }
+    }
+
+    private void Deletion()
+    {
+        var vdPlay = GetComponent<VideoPlayer>();
+        vdPlay.Play();
+        lastBossSpider.transform.parent.gameObject.SetActive(false);
+        medosa.transform.parent.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (1 == butterfly._collectedButterfly)
+        {
+            BossVictory();
+            if (_videoPlay)
+            {
+                if (0 >= _deletionTime) { _videoStop = true; }
+                else { _deletionTime -= Time.deltaTime; }
+            }
+        }
+    }
+}
