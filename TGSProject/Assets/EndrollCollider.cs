9b3e629a4b2f6b2ccb615a38286b6200a63a3b98
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-10-31 01:59:46 +0900

    エンドロール再生用の仮スクリプト

---
+++ TGSProject/Assets/EndrollCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndrollCollider : MonoBehaviour
+{
+    [SerializeField] private FadeController _fade;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            _fade.Fade(false, () => StageConsole.MyLoadScene(StageConsole.MyScene.Endroll));
+        }
+    }
+}
---
+++ TGSProject/Assets/EndrollLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndrollLoader : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D _coll;
+    [SerializeField] private GameObject _audio;
+    [SerializeField] LastEnemy lsEnemy;
+    void Start()
+    {
+        _coll.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lsEnemy.lastBossHp == 0)
+        {
+            _audio.SetActive(false);
+            _coll.gameObject.SetActive(true);
+        }
+    }
+}
