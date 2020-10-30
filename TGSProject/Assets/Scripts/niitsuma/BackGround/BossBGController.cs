7b6d33353b9ed36425c82e5a919d9852d3701d69
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-10-30 14:04:24 +0900

    ボス用背景処理追加, ダメージ処理用別クラス追加

---
+++ TGSProject/Assets/Scripts/niitsuma/BackGround/BossBGController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BossBGController : MonoBehaviour
+{
+    [SerializeField] private Transform _camera;
+    Vector3 delPos;
+    
+
+    void Start()
+    {
+        delPos = new Vector3(_camera.position.x - transform.position.x, 0, _camera.position.z);
+    }
+
+    void LateUpdate()
+    {
+        this.transform.position = _camera.position - delPos;
+    }
+}
---
+++ TGSProject/Assets/Scripts/niitsuma/Damages/DamageStagingControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageStagingControl : MonoBehaviour
+{
+    [SerializeField] private FadeController _fade;
+    [SerializeField] private GameObject _dieImage;
+
+
+    GameManager _gm;
+    bool outCheck = false;
+    bool _fadeInFlag = false;
+    bool _fadeOutFlag = false;
+    bool _bloodClearFlag = false;
+
+    float _time = 0;
+    float _clearTime = 1f;
+
+    void Start()
+    {
+        _gm = GameManager.Instance;
+    }
+    // 死亡演出
+    void DieStageing()
+    {
+        _dieImage.SetActive(true);
+        _fadeOutFlag = false; _fadeInFlag = true;
+        _bloodClearFlag = true;
+    }
+
+    void BloodClear()
+    {
+        if(_time <= _clearTime) { _time += Time.deltaTime; }
+        else { _time = 0; _dieImage.SetActive(false); _bloodClearFlag = false; }
+    }
+    void Update()
+    {
+        if (null != _gm)
+        {
+            if (_gm.GetGameState == GameManager.GameState.Road && !_fadeOutFlag) { outCheck = true; _fadeOutFlag = true; }
+            if (outCheck && !_fadeInFlag) { outCheck = false; _fade.Fade(false, () => DieStageing()); }
+            if (_fadeInFlag) { _fadeInFlag = false; _fade.Fade(true); }
+        }
+        if (_bloodClearFlag) { BloodClear(); }
+    }
+}
