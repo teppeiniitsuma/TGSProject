a265ea39d4353442130135fc792180967ebfe0d9
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2021-02-14 15:51:54 +0900

    タイトル画面でコントローラーかキーボード操作かを切り替える設定画面追加（未完成）

---
+++ TGSProject/Assets/Scripts/niitsuma/Systems/ControllerSystem.cs
@@ -0,0 +1,8 @@
+﻿
+
+public static class ControllerSystem
+{
+    public static bool Controller { get; set; } = false;// キー操作かコントローラー操作かを切り替える
+    public static bool SetSystem { get; set; } = false;// 設定中か否か
+
+}
